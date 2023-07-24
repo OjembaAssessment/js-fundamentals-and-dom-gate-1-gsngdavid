943b9fefd43ce5cf4261c6f6c9d9ada044976dd9
gsngdavid <gsngdavid@gmail.com>
2023-07-24 11:27:55 +0200

    feat: Highlight top five words

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,30 @@
 console.log("Exercise 1 - Word Highlighter");
+
+const text = document.getElementById('myParagraph');
+
+
+function topFiveWords(text) {
+    const newText = text.toLowerCase().replace(/[^\w ']/g, '').replace(/ +/g, ' ').trim();
+    const wordArray = newText.split(' ');
+    const wordCount = wordArray.reduce((obj, cur) => {
+        obj[cur] = obj[cur] >= 1 ? obj[cur] + 1 : 1;
+        return obj;
+    }, {});
+    
+    const wordCountArray = Object.entries(wordCount);
+    const sortedWords = wordCountArray.sort((a, b) => b[1] - a[1]);
+
+    console.log(sortedWords.slice(0, 5));
+
+    return sortedWords.slice(0, 5).map(arr => arr[0]);
+}
+ 
+
+const topFive = topFiveWords(text.innerHTML);
+
+text.innerHTML = text.innerHTML.replace(/\w+/g, match => {
+    if (topFive.includes(match)) {
+        return `<span style='background-color: red'>${match}</span>`
+    }
+    return match;
+});
