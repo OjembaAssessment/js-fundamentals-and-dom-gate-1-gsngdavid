9ce4bb38b6cee6610cd6ae004d3fb17749e3a2ac
gsngdavid <gsngdavid@gmail.com>
2023-07-24 11:43:12 +0200

    feat: Underline words starting with capital letter

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -14,17 +14,24 @@ function topFiveWords(text) {
     const wordCountArray = Object.entries(wordCount);
     const sortedWords = wordCountArray.sort((a, b) => b[1] - a[1]);
 
-    console.log(sortedWords.slice(0, 5));
-
     return sortedWords.slice(0, 5).map(arr => arr[0]);
 }
  
 
 const topFive = topFiveWords(text.innerHTML);
 
-text.innerHTML = text.innerHTML.replace(/\w+/g, match => {
-    if (topFive.includes(match)) {
-        return `<span style='background-color: red'>${match}</span>`
+text.innerHTML = text.innerHTML.replace(/\w+/ig, match => {
+    if (topFive.includes(match.toLowerCase())) {
+        return `<span style='background-color: rgba(0, 0, 0, 0.2)'>${match}</span>`
     }
     return match;
+});
+
+const highlighted = document.querySelectorAll('[style]');
+
+highlighted.forEach(el => {
+    const firstChar = el.textContent[0];
+    if(firstChar === firstChar.toUpperCase()) {
+        el.style.textDecoration = 'underline';
+    }
 });
